0dc58eef749dadeff797bf06ee15f107f68c2a24
Cameron <cameron.p.buckingham@gmail.com>
2014-07-08 11:04:03 -0600

    geolocator

---
+++ geoLocator/config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+
+# Be sure to restart your server when you modify this file.
+
+GeoLocator::Application.config.session_store :cookie_store, key: '_geoLocator_session'
---
+++ gohuntgeo.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+
+class GoHuntGeoApp < Sinatra::Base
+
+  get '/' do
+    erb :root
+  end
+
+  get '/login-sign_up' do
+    erb :login_sign_up
+  end
+
+  get '/user_page' do
+    erb :user_page
+  end
+
+  get '/how_to_start' do
+    erb :how_to_start
+  end
+
+  get '/how_this_works' do
+    erb :how_this_works
+  end
+
+  get '/why' do
+    erb :why
+  end
+
+  get '/contact_us' do
+    erb :contact_us
+  end
+
+  run! if app_file == $0
+
+end
+
