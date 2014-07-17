b275088a03fa8d6c2218d9143aebdc91e7434835
Cameron <cameron.p.buckingham@gmail.com>
2014-07-17 15:17:14 -0600

    flag pics

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "https://rubygems.org"
 
 # gem "rails"
 
-gem "rerun", '~>0.10.0'
+gem "rerun"
---
+++ locators_controller.rb
@@ -0,0 +1,31 @@
+require 'simple_geolocation'
+
+class LocatorsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :new_request
+
+  def index
+    ip = request.remote_ip
+    if ip == '127.0.0.1'
+      ip = '74.125.113.104'
+    end
+    @location = get_my_location(ip)
+  end
+
+  def new_request
+    ip = params[:ip_input]
+
+    if ip.blank? || ip !~ /\d+\.\d+\.\d+\.\d+/
+      ip = '74.125.113.104'
+    end
+
+    @location = get_my_location(ip)
+
+    render :index
+  end
+
+  private 
+
+  def get_my_location(ip)
+    SimpleGeolocation::Geocoder.new(ip).geocode!
+  end
+end
